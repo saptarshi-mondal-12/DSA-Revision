c5ee0207f4784dc3049e27333bfba2ce2792b3ac
saptarshi mondal <saptarishimondal@gmail.com>
2025-08-02 11:18:52 +0530

    Updates 2025

--- 8. Subarray & Subsequence & Sliding Window & 2-Pointer/26_Fruit_into_basket_AMAZON.cpp
+++ 8. Subarray & Subsequence & Sliding Window & 2-Pointer/26_Fruit_into_basket_AMAZON.cpp
@@ -2,7 +2,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-/* FRUIT INTO BASKETS
+/* FRUIT INTO BASKETS - AMAZON 2025
 
 You are visiting a farm that has a single row of fruit trees arranged from left to right. The trees are represented by an integer array of arr[], where arr[i]  is the type of fruit the ith tree produces.
 You want to collect as much fruit as possible. However, the owner has some strict rules that you must follow :
